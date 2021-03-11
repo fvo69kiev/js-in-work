2f3b68263de1681cabbcf439a402b3f9bd7efc83
Victor Foryi <foryi@v.rada.gov.ua>
2021-03-11 17:26:36 +0200

    Create script sending data \from Forms\ to server - MAMP or Open server

--- js/script.js
+++ js/script.js
@@ -102,9 +102,9 @@ window.addEventListener('DOMContentLoaded', () => {
         modal = document.querySelector('.modal');
 
     function openModal() {
-        // modal.classList.add('show');
-        // modal.classList.remove('hide');
-        modal.classList.toggle('show');  // You can change add + remove or this toggle
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        // modal.classList.toggle('show');  // You can change add + remove or this toggle
         document.body.style.overflow = 'hidden';
         clearInterval(modalTimerId);
     }
@@ -114,9 +114,9 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     function closeModal() {
-        // modal.classList.add('hide');
-        // modal.classList.remove('show');
-        modal.classList.toggle('show');  // You can change add + remove or this toggle
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.classList.toggle('show');  // You can change add + remove or this toggle
         document.body.style.overflow = '';
     }
 
@@ -213,6 +213,82 @@ window.addEventListener('DOMContentLoaded', () => {
         16,
         ".menu .container",
     ).render();
+
+
+    // Forms----------------------------------------------------------------------
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с Вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;               
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key){
+                object[key] = value;
+            });
+
+            fetch('server1.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>&#10006;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
 
 
 

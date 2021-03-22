bd74b4cc6718a0a64edef5a3750485823881085b
Victor Foryi <foryi@v.rada.gov.ua>
2021-03-22 17:04:39 +0200

    Create Menu & ScrollTop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
 
 
     // Create Tabs-----------------------------------------------------------------------------
@@ -560,8 +560,52 @@ window.addEventListener('DOMContentLoaded', () => {
     getDynamicInformation('#age');
 
 
+    // Create Menu-------------------------------------------------
+        // Select all of anchors; Set animation time & quantity frames-----
+    const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+        animationTime = 700,
+        framesCount = 20;
+
+    anchors.forEach(function(item) {
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+            let scroller = setInterval(function() {
+                let scrollBy = coordY / framesCount;
+                if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                    window.scrollBy(0, scrollBy);
+                } else {
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+            }, animationTime / framesCount);
+        });
+    });
+
+        // To top---------------------------------------
+    const toTop = document.querySelector('#toTop');
+    window.onscroll = () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 100) {
+            toTop.style.display = "block";
+        } else {
+            toTop.style.display = "none";
+        }
+    }
+
+    var t;
+    function up() {
+        const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+        if(top > 0) {
+            window.scrollBy(0,-100);
+            t = setTimeout('up()',20);
+        } else clearTimeout(t);
+        return false;
+    }
+
+    toTop.addEventListener('click', up);
 
 
 
-
-});
+// });
